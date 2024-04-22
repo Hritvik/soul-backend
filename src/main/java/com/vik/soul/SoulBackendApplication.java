d38509835f5091f3d5c4b3f0436537fce5e98568
hritvik <hritvikdinkar@gmail.com>
2024-04-22 09:57:56 +0530

    init

---
+++ src/main/java/com/vik/soul/SoulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vik.soul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vik/soul/SoulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vik.soul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
